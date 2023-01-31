d245a81d26fdf98c1cfdf33134ae29bebc42d761
강효진 <khj6354@naver.com>
2023-02-01 01:20:51 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 292 ms, Memory: 18168 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -6,9 +6,10 @@ import java.io.OutputStreamWriter;
 public class Main {
 	
 	static int N, K;
-	static int belt;
+	static int beltLen;
 	static int[] durabilities;
 	static boolean[] robots;
+	static int count0;
 	
 	public static void main(String[] args) throws Exception {
 
@@ -17,14 +18,14 @@ public class Main {
 		
 		//초기화
 		String[] line = br.readLine().split(" ");
-		N = Integer.parseInt(line[0]); //컨베이어 벨트 길이
+		N = Integer.parseInt(line[0]); //컨베이어 벨트 1개의 길이
 		K = Integer.parseInt(line[1]); //내구도가 0인 칸의 개수
 		
 		line = br.readLine().split(" ");
-		belt = 2 * N; //컨베이어 벨트 2개의 칸 개수
-		durabilities = new int[belt]; //칸의 내구도
+		beltLen = 2 * N; //컨베이어 벨트 2개의 길이
+		durabilities = new int[beltLen]; //칸의 내구도
 		
-		for (int i = 0; i < belt; i++) {
+		for (int i = 0; i < beltLen; i++) {
 			durabilities[i] = Integer.parseInt(line[i]);
 		}
 		
@@ -39,7 +40,7 @@ public class Main {
 			seq2();
 			seq3();
 			step++;
-		} while (countDurability0() < K);
+		} while (count0 < K);
 		
 		
 		//출력
@@ -54,7 +55,7 @@ public class Main {
 		//벨트 회전
 		int prev = durabilities[0];
 		
-		for (int i = 1; i < belt; i++) { //2N - 1까지 뒤로 밀기
+		for (int i = 1; i < beltLen; i++) { //2N - 1까지 뒤로 밀기
 			int cur = durabilities[i];
 			durabilities[i] = prev;
 			prev = cur;
@@ -81,7 +82,10 @@ public class Main {
 			if (robots[i] && !robots[i + 1] && durabilities[i + 1] != 0) { //자리에 로봇이 있고, 이동하려는 칸에 로봇이 없고, 그 칸의 내구도가 0이 아닐 때
 				robots[i] ^= true; //내 칸 지우고
 				robots[i + 1] ^= true; //한 칸 이동
-				durabilities[i + 1]--; //내구도 - 1
+				
+				if (--durabilities[i + 1] == 0) { //내구도 - 1
+					count0++;
+				}
 			}
 		}
 		
@@ -93,20 +97,11 @@ public class Main {
 	static void seq3() {
 		if (durabilities[0] != 0) { //올리는 위치에 있는 칸의 내구도가 0이 아니면
 			robots[0] = true; //로봇 올린다.
-			durabilities[0]--;
-		}
-	}
-	
-	static int countDurability0() { //내구도 0인 칸 개수 세기
-		int cnt = 0;
-		
-		for (int i = 0; i < belt; i++) {
-			if (durabilities[i] == 0) {
-				cnt++;
+			
+			if (--durabilities[0] == 0) { //내구도 - 1
+				count0++;
 			}
 		}
-		
-		return cnt;
 	}
 
 }
